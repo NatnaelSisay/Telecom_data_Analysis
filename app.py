367962c78a6d46f878c0010ac4307d476e547b71
NatnaelSisay <natyjava8@gmail.com>
2021-07-17 10:08:18 +0300

    started dashboard

---
+++ app.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+st.set_page_config(page_title="Day 5", layout="wide")
+
+
+
+st.title("TelCo Data analysis")
+st.sidebar.markdown("# Side Bar")
